4c9315eaa1dca5328d73437fbbaf394a09aa4b13
KASHISH006218 <sahukashish436@gmail.com>
2025-05-07 13:42:30 +0530

    js

---
+++ main.java
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+class Parent{
+   void main( int a, int b){
+      System.out.println(a+b);
+   }
+   void main(int a, int b, int c){
+      System.out.println(a+b+c);
+   }
+}
+class main{
+   public static void main(String[] args) {
+       Parent obj = new Parent();
+       obj.main(5, 10);
+
+       obj.main(5,10,15);
+   }
+}
