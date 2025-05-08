a3491e357b0d42100fb7690cb87f225753c234f0
KASHISH006218 <sahukashish436@gmail.com>
2025-05-08 10:41:05 +0530

    msg

--- main.java
+++ main.java
@@ -1,24 +1,35 @@
 
 
-
-
-
-
-
-
-class Parent{
-   void main( int a, int b){
-      System.out.println(a+b);
-   }
-   void main(int a, int b, int c){
-      System.out.println(a+b+c);
-   }
-}
 class main{
-   public static void main(String[] args) {
-       Parent obj = new Parent();
-       obj.main(5, 10);
+    public static void main(String[] args) {
+        Payment obj = new UpiPayment();
+         obj.pay(5000);
+         obj.print();
 
-       obj.main(5,10,15);
-   }
+         Payment obj1= new CardPayment();
+         obj1.pay(11000);
+         obj1.print();
+    }
 }
+
+    
+    
+    
+abstract class Payment{
+    abstract void pay(int a);
+    void print(){
+        System.out.println("x:payment done");
+
+    }
+}
+class UpiPayment extends Payment{
+    void pay(int a){
+        System.out.println("payment done via Upi" + a);
+    }
+}
+class CardPayment extends Payment{
+    void pay(int a){
+        System.out.println("CardPayment of" + a);
+    }
+}
+
